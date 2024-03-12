43a8adc3c058125a1413403f7c84f224d2d76414
Gabriel Guerra <112008289+Guerragga@users.noreply.github.com>
2024-03-12 00:06:40 -0300

    layout update
    In this update the entire structure was changed based on a new design produced by me, it is not yet complete.
    Hope you like it.

---
+++ script.js
@@ -0,0 +1,7 @@
+      let typingText = new Typed("#text", {
+      strings : ["Front-End Developer", "UX/UI Design", "Mobile Design"],
+      loop : true,
+      typeSpeed : 100,
+      backSpeed : 50,
+      backDelay : 1000
+    })
