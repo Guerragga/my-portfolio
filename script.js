0ce7de84e500cdc4ab54390797a9fb1df3692cfd
Gabriel Guerra <112008289+Guerragga@users.noreply.github.com>
2026-05-26 01:31:56 -0300

    Refina visual do portfólio com identidade mais moderna e alinhada ao LinkedIn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-      let typingText = new Typed("#text", {
-      strings : ["Front-End Developer", "UX/UI Design", "Mobile Design"],
-      loop : true,
-      typeSpeed : 100,
-      backSpeed : 50,
-      backDelay : 1000
-    })
+let typingText = new Typed("#text", {
+  strings: ["Front-End Developer", "UX/UI Designer", "Criador de Experiências Digitais"],
+  loop: true,
+  typeSpeed: 90,
+  backSpeed: 45,
+  backDelay: 1200,
+});
